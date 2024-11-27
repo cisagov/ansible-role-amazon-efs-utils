7539f7e0b9132bd51668d5baa6bb9b2efb9aafc6
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-11-27 17:14:54 -0500

    Take into account splitting of openssl-devel-engine into separate package
    This is now the case on Fedora 41 and later.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,8 +16,19 @@ def test_packages(host):
     """Test that the appropriate packages were installed."""
     distribution = host.system_info.distribution
     codename = host.system_info.codename
+    release = host.system_info.release
     if distribution in ["fedora"]:
-        pkgs = ["amazon-efs-utils", "cargo", "make", "openssl-devel", "rpm-build"]
+        if any([release.startswith(version) for version in ["39", "40"]]):
+            pkgs = ["amazon-efs-utils", "cargo", "make", "openssl-devel", "rpm-build"]
+        else:
+            pkgs = [
+                "amazon-efs-utils",
+                "cargo",
+                "make",
+                "openssl-devel",
+                "openssl-devel-engine",
+                "rpm-build",
+            ]
     elif distribution in ["debian", "kali", "ubuntu"]:
         # TODO - Install version 2.x on Ubuntu Jammy and Noble when
         # possible.  See #53 for more details.
