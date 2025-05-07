f85b83b11820c11bc488816fe150696e67824b62
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-05-07 11:34:38 -0400

    Use version 1.x of aws/efs-utils for Ubuntu Focal (20.04)
    Version 2.x does not currently build on this platform.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -28,7 +28,7 @@ def test_packages(host):
     elif distribution in ["debian", "kali", "ubuntu"]:
         # TODO - Install version 2.x on Ubuntu Jammy and Noble when
         # possible.  See #53 for more details.
-        if codename in ["buster", "bullseye", "bookworm", "jammy", "noble"]:
+        if codename in ["buster", "bullseye", "bookworm", "focal", "jammy", "noble"]:
             pkgs = ["amazon-efs-utils", "binutils", "make"]
         else:
             pkgs = [
